adea338feae75b0967197f24121e2fa98be21164
etempleton <erictempleton1@gmail.com>
2018-08-30 06:00:40 -0400

    creating new container component

---
+++ tracker_frontend/src/containers/tables/WorkoutTable.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import Table from '../../components/table/Table.jsx';
+
+
+const headers = ['Header1', 'Header2', 'Header3'];
+const tableRows = [
+  ['cell1', 'cell2', 'cell3',],
+  ['cell4', 'cell5', 'cell6',],
+  ['cell7', 'cell8', 'cell9',],
+  ['cell10', 'cell11', 'cell12',],
+];
+
+function WorkoutTable() {
+  return (
+    <div className="col-md-6">
+      <Table headers={headers} tableRows={tableRows} />
+    </div>
+  )
+}
+
+
+export default WorkoutTable;
