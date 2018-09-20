00495ef00fc4a4e217727b1d68cbf1f87f1bfc0c
etempleton <erictempleton1@gmail.com>
2018-09-20 05:38:37 -0400

    fixing route typo

--- tracker_frontend/src/index.js
+++ tracker_frontend/src/index.js
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import registerServiceWorker from './registerServiceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
 
 import Dashboard from './components/dashboard/Dashboard';
+import CueSheet from './components/cue_sheet/CueSheet';
 
 
 ReactDOM.render(
-    <Router>
-        <Route exact path="/" component={Dashboard} />
-    </Router>,
+  <Router>
+    <Fragment>
+      <Route exact path="/" component={Dashboard} />
+      <Route exact path="/cuesheet" component={CueSheet} />
+    </Fragment>
+  </Router>,
 document.getElementById('root'));
 registerServiceWorker();
