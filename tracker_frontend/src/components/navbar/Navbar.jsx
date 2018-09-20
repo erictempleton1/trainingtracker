d33e9b8dd192ccfdce2586e2d47841c2aec0c8a8
etempleton <erictempleton1@gmail.com>
2018-09-20 05:38:59 -0400

    removing button from right

--- tracker_frontend/src/components/navbar/Navbar.jsx
+++ tracker_frontend/src/components/navbar/Navbar.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 
-import Button from '../buttons/Button';
 import './navbarStyles.css';
 
 
@@ -9,9 +8,6 @@ function Navbar(props) {
   return (
     <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
       <a className="navbar-brand col-sm-3 col-md-2 mr-0" href="#">{brand}</a>
-      <span className="navbar-text">
-        <Button>Upload File</Button>
-      </span>
     </nav>
   )
 }
